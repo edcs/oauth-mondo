af369ccb62a84e0c22ecff3074fceec2c7559ffe
Edward Coleridge Smith <edward.smith@pod-point.com>
2016-03-05 14:27:11 +0000

    Added test suite

---
+++ test/src/Client/MondoTest.php
@@ -0,0 +1,159 @@
+<?php
+namespace Edcs\OAuth2\Client\Client\Test;
+
+use Edcs\OAuth2\Client\Provider\Mondo;
+use Mockery as m;
+use PHPUnit_Framework_TestCase;
+
+class MondoTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * Instance of the mondo provider.
+     *
+     * @var Mondo
+     */
+    protected $provider;
+
+    /**
+     * Creates a new instance of the Mondo provider before each test.
+     *
+     * @before
+     */
+    protected function setUpProvider()
+    {
+        $this->provider = new Mondo([
+            'clientId' => 'mock_client_id',
+            'clientSecret' => 'mock_secret',
+            'redirectUri' => 'none',
+        ]);
+    }
+
+    /**
+     * Ensures that the authorization url is correctly generated.
+     */
+    public function testAuthorizationUrl()
+    {
+        $url = $this->provider->getAuthorizationUrl();
+
+        $uri = parse_url($url);
+        parse_str($uri['query'], $query);
+
+        $this->assertArrayHasKey('client_id', $query);
+        $this->assertArrayHasKey('redirect_uri', $query);
+        $this->assertArrayHasKey('state', $query);
+        $this->assertArrayHasKey('scope', $query);
+        $this->assertArrayHasKey('response_type', $query);
+        $this->assertArrayHasKey('approval_prompt', $query);
+
+        $this->assertNotNull($this->provider->getState());
+    }
+
+    /**
+     * Ensures that scopes can be added to the authorization url.
+     */
+    public function testScopes()
+    {
+        $options = ['scope' => [uniqid(), uniqid()]];
+        $url = $this->provider->getAuthorizationUrl($options);
+
+        $this->assertContains(urlencode(implode(',', $options['scope'])), $url);
+    }
+
+    /**
+     * Ensures that the authorization url is correctly generated.
+     */
+    public function testGetAuthorizationUrl()
+    {
+        $url = $this->provider->getAuthorizationUrl();
+        $uri = parse_url($url);
+
+        $this->assertEquals('auth.getmondo.co.uk', $uri['host']);
+    }
+
+    /**
+     * Ensures that the base access token url is correctly generated.
+     */
+    public function testGetBaseAccessTokenUrl()
+    {
+        $params = [];
+        $url = $this->provider->getBaseAccessTokenUrl($params);
+        $uri = parse_url($url);
+
+        $this->assertEquals('/oauth2/token', $uri['path']);
+    }
+
+    /**
+     * Ensures that an access token is correctly generated.
+     */
+    public function testGetAccessToken()
+    {
+        $response = m::mock('Psr\Http\Message\ResponseInterface');
+        $response->shouldReceive('getBody')->andReturn('{"access_token": "mock_access_token", "client_id": "mock_client_id", "refresh_token": "mock_refresh_token", "token_type": "Bearer"}');
+        $response->shouldReceive('getHeader')->andReturn(['content-type' => 'json']);
+        $response->shouldReceive('getStatusCode')->andReturn(200);
+
+        $client = m::mock('GuzzleHttp\ClientInterface');
+        $client->shouldReceive('send')->times(1)->andReturn($response);
+
+        $this->provider->setHttpClient($client);
+
+        $token = $this->provider->getAccessToken('authorization_code', ['code' => 'mock_authorization_code']);
+
+        $this->assertEquals('mock_access_token', $token->getToken());
+        $this->assertEquals('mock_refresh_token', $token->getRefreshToken());
+        $this->assertNull($token->getExpires());
+        $this->assertNull($token->getResourceOwnerId());
+    }
+
+    /**
+     * Ensures that valid resource owner information is returned.
+     */
+    public function testUserData()
+    {
+        $userId = rand(1000, 9999);
+
+        $postResponse = m::mock('Psr\Http\Message\ResponseInterface');
+        $postResponse->shouldReceive('getBody')->andReturn('access_token=mock_access_token&expires=3600&refresh_token=mock_refresh_token&otherKey={1234}');
+        $postResponse->shouldReceive('getHeader')->andReturn(['content-type' => 'application/x-www-form-urlencoded']);
+        $postResponse->shouldReceive('getStatusCode')->andReturn(200);
+
+        $userResponse = m::mock('Psr\Http\Message\ResponseInterface');
+        $userResponse->shouldReceive('getBody')->andReturn('{"authenticated": true,"client_id": "client_id","user_id": "' . $userId . '"}');
+        $userResponse->shouldReceive('getHeader')->andReturn(['content-type' => 'json']);
+        $userResponse->shouldReceive('getStatusCode')->andReturn(200);
+
+        $client = m::mock('GuzzleHttp\ClientInterface');
+        $client->shouldReceive('send')->times(2)->andReturn($postResponse, $userResponse);
+
+        $this->provider->setHttpClient($client);
+
+        $token = $this->provider->getAccessToken('authorization_code', ['code' => 'mock_authorization_code']);
+
+        $user = $this->provider->getResourceOwner($token);
+
+        $this->assertEquals($userId, $user->getId());
+    }
+
+    /**
+     * Ensures that an exception is thrown when an error is received.
+     *
+     * @expectedException \League\OAuth2\Client\Provider\Exception\IdentityProviderException
+     */
+    public function testExceptionThrownWhenErrorObjectReceived()
+    {
+        $message = uniqid();
+        $status = rand(400, 600);
+
+        $postResponse = m::mock('Psr\Http\Message\ResponseInterface');
+        $postResponse->shouldReceive('getBody')->andReturn(' {"message":"'.$message.'"}');
+        $postResponse->shouldReceive('getHeader')->andReturn(['content-type' => 'json']);
+        $postResponse->shouldReceive('getStatusCode')->andReturn($status);
+
+        $client = m::mock('GuzzleHttp\ClientInterface');
+        $client->shouldReceive('send')->times(1)->andReturn($postResponse);
+
+        $this->provider->setHttpClient($client);
+
+        $this->provider->getAccessToken('authorization_code', ['code' => 'mock_authorization_code']);
+    }
+}
