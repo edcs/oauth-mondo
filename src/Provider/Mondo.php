9d80b5874c098848b018aace9844c6255e592267
Edward Coleridge Smith <edward.smith@pod-point.com>
2016-03-17 17:24:25 +0000

    Fixed namespacing bug

--- src/Provider/Mondo.php
+++ src/Provider/Mondo.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Edcs\Oauth2\Client\Provider;
+namespace Edcs\OAuth2\Client\Provider;
 
 use League\OAuth2\Client\Provider\AbstractProvider;
 use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
